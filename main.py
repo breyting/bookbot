a4a30808efbf1286241b9917c02724b9255d9380
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-01-07 21:10:03 +0100

    add the function count_character

--- main.py
+++ main.py
@@ -8,4 +8,16 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    lower_text = text.lower()
+    number_of_each_character = {}
+
+    for character in lower_text:
+        if character in number_of_each_character:
+            number_of_each_character[character] += 1
+        else:
+            number_of_each_character[character] = 1
+    
+    return number_of_each_character
+
 main()
