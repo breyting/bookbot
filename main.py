a4dc849b30eb44f367fab38735a9d5e738be6ce7
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-01-07 21:03:29 +0100

    change to print word count

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main ():
     with open("books/frankenstein.txt") as file:
         text = file.read()
-    print(text)
+        print(count_words(text))
 
+
+def count_words(text):
+    words = text.split()
+    return len(words)
 
 main()
