86a45f98ebc8a5a5d3c17698186594abd921191a
Matias Gathoye <89335417+breyting@users.noreply.github.com>
2025-01-08 08:12:03 +0100

    ajout de print report (a terminer), doit encore supprimer le max character le la liste

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main ():
     with open("books/frankenstein.txt") as file:
         text = file.read()
-        print(count_words(text))
+        words = count_words(text)
+        characters = count_characters(text)
+        print_report(words, characters)
 
 
 def count_words(text):
@@ -11,13 +13,31 @@ def count_words(text):
 def count_characters(text):
     lower_text = text.lower()
     number_of_each_character = {}
+    set_of_characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     for character in lower_text:
         if character in number_of_each_character:
             number_of_each_character[character] += 1
-        else:
+        elif character in set_of_characters:
             number_of_each_character[character] = 1
     
     return number_of_each_character
+
+def print_report(words, characters):
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{words} words found in the document")
+    print("")
+
+    charachter_sorted = {}
+    for i in range(0, len(characters)):
+        max = float("-inf")
+        max_character = ""
+
+        for charachter in characters:
+            if characters[charachter] > max:
+                max = characters[charachter]
+                max_character = charachter
+
+        print(f"The \'{max_character}\' charachter was found {max} times")
 
 main()
